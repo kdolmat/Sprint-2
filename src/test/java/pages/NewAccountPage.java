7d0604bafef8119a9978c39ec212853b85806996
Olga K <kobrochka777@gmail.com>
2022-04-18 00:42:11 -0400

    Added Created Account and Recipe Page

---
+++ src/test/java/pages/NewAccountPage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class NewAccountPage {
+
+    //Non-pageFactory style page object class
+
+//    public WebElement registerPage = Driver.getDriver().findElement(By.partialLinkText("Register"));
+//    public WebElement email = Driver.getDriver().findElement(By.id("email"));
+//    public WebElement password = Driver.getDriver().findElement(By.id("password"));
+
+
+    //PageFactory style
+
+    public NewAccountPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+        @FindBy (partialLinkText = "Register")
+        public WebElement registerPage;
+
+        @FindBy (id = "email")
+         public WebElement email;
+
+       @FindBy (id = "password")
+       public WebElement password;
+
+
+}
---
+++ src/test/java/tests/CreateAccountTest.java
@@ -0,0 +1,32 @@
+package tests;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.NewAccountPage;
+import utilities.PropertyReader;
+import utilities.SeleniumUtils;
+
+public class CreateAccountTest extends TestBase {
+
+    @Test(groups = {"smoke"})
+    public void negativeLoginTest() {
+        driver.get(PropertyReader.getProperty("url"));
+
+
+
+        NewAccountPage newAcc = new NewAccountPage();
+        newAcc.registerPage.click();
+        newAcc.email.sendKeys(PropertyReader.getProperty("username"));
+        SeleniumUtils.waitFor(2);
+        newAcc.password.sendKeys(PropertyReader.getProperty("password"));
+
+
+        Assert.assertTrue(driver.getPageSource().contains("This email already belongs to an account. "));
+
+
+
+
+    }
+
+
+}
