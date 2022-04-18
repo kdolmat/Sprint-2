9a7f5c094b59e7f08ae2fa73086ddf4150a9432b
Kiryl <incaseofsomethingspecial@gmail.com>
2022-04-18 01:30:18 -0400

    CartUpdate

--- src/test/java/tests/Cart.java
+++ src/test/java/tests/Cart.java
@@ -15,45 +15,44 @@ public class Cart extends TestBase{
 
 
     @Test(priority=1)
-    public void Login() throws InterruptedException {
-
-        LoginPage loginPage = new LoginPage();
+    public void Login(){
 
         new LoginPage().loginWithValidCredentials();
         SeleniumUtils.waitFor(2);
         Assert.assertEquals(driver.getTitle(), title);
     }
     @Test(priority=2)
-    public void cartMatch() throws InterruptedException {
-
+    public void cartMatch(){
+        new LoginPage().loginWithValidCredentials();
         CartPage cartPage = new CartPage();
 
-        new LoginPage().loginWithValidCredentials();
+
         SeleniumUtils.waitFor(2);
         cartPage.cartButton();
         String expected = " Steam Table ";
         List<String>a= Arrays.asList(" Steam Table ");
         List<String> actual = SeleniumUtils.getElementsText(cartPage.cartItems);
-        Assert.assertTrue(actual.contains(a));//doesn't match
+        Assert.assertTrue(a.contains(actual));//doesn't match
     }
      @Test(priority=3)
-    public void thirdElementQuantity() throws InterruptedException {
+    public void thirdElementQuantity(){
 
+        new LoginPage().loginWithValidCredentials();
         CartPage cartPage = new CartPage();
-         new LoginPage().loginWithValidCredentials();
 
-        // SeleniumUtils.waitFor(2);
+
+         SeleniumUtils.waitFor(2);
          cartPage.cartButton();
          SeleniumUtils.waitFor(2);
-         //cartPage.thirdEl();
+         cartPage.thirdEl();
 
     }
 
      @Test(priority=4)
-    public void prices() throws InterruptedException {
-
-         CartPage cartPage = new CartPage();
+    public void prices(){
          new LoginPage().loginWithValidCredentials();
+         CartPage cartPage = new CartPage();
+
 
          SeleniumUtils.waitFor(2);
          cartPage.cartButton();
@@ -62,7 +61,7 @@ public class Cart extends TestBase{
     }
 
     @Test(priority=5)
-    public void checkOut() throws InterruptedException {
+    public void checkOut(){
 
         new LoginPage().loginWithValidCredentials();
         CartPage cartPage = new CartPage();
